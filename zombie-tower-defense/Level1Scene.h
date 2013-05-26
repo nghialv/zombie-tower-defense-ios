3b6bc020def6629d25104c99d60a0c4ddb5996e0
lengoccuong <lncnb91@gmail.com>
2013-05-26 18:09:47 +0900

    Zombie added

--- zombie-tower-defense/Level1Scene.h
+++ zombie-tower-defense/Level1Scene.h
@@ -15,6 +15,8 @@ using namespace cocos2d;
 
 class Level1 : public CCLayer
 {
+    int delay = 0;
+    
 public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
@@ -29,13 +31,17 @@ public:
     virtual void ccTouchEnded(CCTouch* touch, CCEvent* event);
     virtual void ccTouchCancelled(CCTouch* touch, CCEvent* event);
     virtual void ccTouchMoved(CCTouch* touch, CCEvent* event);
+    int flag;
+    void update(float dt);
     
 private:
     CCTMXTiledMap *tileMap;
     CCTMXLayer *background;
+    cocos2d::CCArray *_zombies;
     
     // add cannon callback
     void addCannonCallback(CCObject* pSender);
+    void addZombie(CCNode *sender);
 };
 
 #endif /* defined(__zombie_tower_defense__Level1Scene__) */
